2f43129c1cf6d963384e096a62f1f866a39dd262
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-02-02 20:57:00 +0100

    Implemented web sockets

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ChatService } from './shared/chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
-
-  constructor() { }
+export class ChatComponent implements OnInit, OnDestroy {
+  message = new FormControl('');
+  messages: string[] = [];
+  private sub: Subscription;
+  constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
+    this.sub = this.chatService.listenForMessages()
+      .subscribe(message => {
+        console.log('hello');
+        this.messages.push(message);
+      });
   }
 
+  sendMessage(): void {
+    console.log(this.message.value);
+    this.chatService.sendMessage(this.message.value);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroyed');
+    if (this.sub){
+      this.sub.unsubscribe();
+    }
+  }
 }
